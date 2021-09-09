dab61179e602c9bdb45b168a45dec8b144def8ba
Alexander0076 <uliseselias007@gmail.com>
2021-09-09 00:51:48 -0600

    Archivo completo

---
+++ WpfApp1/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Persona
+    {
+        private string nombre;
+        private string cargo;
+        private int edad;
+        private double sueldo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Cargo { get => cargo; set => cargo = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+    }
+}
